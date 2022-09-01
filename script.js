71750284e5b596bde022e6a7cf68a658ac99e723
colebell <colebell01@hotmail.com>
2022-08-31 20:57:55 -0700

    modified:   script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 
+
+
+function orderInput(theArray) {
+    setTimeout(() => {
+       
+        for (let i = 0; i < theArray.length; i++) { 
+            
+        }
+
+    }, 1000);
+}
+
+let myArray = ["A", "B", "C"]
+orderInput(myArray)

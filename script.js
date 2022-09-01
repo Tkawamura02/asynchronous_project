70b62e34d8c6545af31df61dd8c1f0408f0f9292
colebell <colebell01@hotmail.com>
2022-08-31 22:30:32 -0700

    modified:   script.js

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
-
-
-
-function orderInput(theArray) {
-    setTimeout(() => {
-       
-        for (let i = 0; i < theArray.length; i++) { 
-            
-        }
-
-    }, 1000);
+function printMessage() {
+    console.log("Please wait while we sort this out...")
 }
 
-let myArray = ["A", "B", "C"]
-orderInput(myArray)
+function orderInput(theArray, call) {
+    setTimeout(() => {
+
+        let newArray = []
+        let savedIndex = 0
+        let x = theArray.length
+        
+        for (let k = 0; k < theArray.length; k++) {
+        		theArray[k] = theArray[k].toUpperCase();
+        }
+        
+       
+        for (let i = 0; i < x -1; i++) { 
+            for (let j = 0; j < theArray.length; j++) {
+                if (theArray[j] <= theArray[i]) {
+                	if(theArray[j] <= theArray[savedIndex])
+                    savedIndex = j
+                }
+            }
+            //assign next lowest num
+            newArray[i] = theArray[savedIndex]
+            theArray.splice(savedIndex, 1)
+            
+        }
+        
+        newArray.push(theArray[0])
+        
+        console.log(newArray)
+
+    }, 3000);
+
+    call()
+
+}
+
+console.log("Starting now...")
+let myArray = ["a", "b", "e", "q", "t", "t"]
+orderInput(myArray, printMessage)
